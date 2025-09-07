e9ed15d898aff6fe7b55dd2abf2e24dc0f74706f
Mankrit Singh <singhmankrit@gmail.com>
2025-09-07 23:22:00 +0200

    double shooters

--- platformer_test/scripts/shooter.gd
+++ platformer_test/scripts/shooter.gd
@@ -1,10 +1,14 @@
 extends Node2D
 
 @export var bullet_scene: PackedScene = preload("res://scenes/bullet.tscn")
-@export var fire_rate: float = 0.75   # seconds between shots
-@export var bullet_offset: Vector2 = Vector2(-10, 0)
+@export var fire_rate: float = 0.5   # seconds between shots
+@export var bullet_offset: Vector2 = Vector2(10, 0)
+@export var direction := 1  # 1 = right, -1 = left
 
 var timer := 0.0
+
+func _ready():
+	$AnimatedSprite2D.flip_h = (direction == -1)
 
 func _process(delta: float) -> void:
 	timer += delta
@@ -15,5 +19,6 @@ func _process(delta: float) -> void:
 func fire_bullet():
 	if bullet_scene:
 		var bullet = bullet_scene.instantiate()
-		bullet.global_position = global_position + bullet_offset
+		bullet.global_position = global_position + direction*bullet_offset
+		bullet.direction = direction
 		get_tree().current_scene.add_child(bullet)
