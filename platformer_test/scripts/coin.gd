b4497b885c63318237836e0d25540d1f42f05f2a
Mankrit Singh <singhmankrit@gmail.com>
2025-09-11 12:45:40 +0200

    Revert "Made measurement pad"
    This reverts commit 86caff2cb335fbeca54eb2e400060099a7d8564a.

--- platformer_test/scripts/coin.gd
+++ platformer_test/scripts/coin.gd
@@ -8,4 +8,5 @@ func _on_body_entered(body: Node2D) -> void:
 	# Detects if player touches coin
 	var state = game_manager.measure()
 	if (body.is_state_zero and state==0) or (not body.is_state_zero and state == 1):
-		game_manager.spawn_point = self.global_position
+		game_manager.add_point()
+		animation_player.play("pickup")
