86caff2cb335fbeca54eb2e400060099a7d8564a
aronintendo <battarionwars@live.nl>
2025-09-11 12:38:35 +0200

    Made measurement pad
    I made the measurement pad in about the same fashion as the coin

--- platformer_test/scripts/coin.gd
+++ platformer_test/scripts/coin.gd
@@ -8,5 +8,4 @@ func _on_body_entered(body: Node2D) -> void:
 	# Detects if player touches coin
 	var state = game_manager.measure()
 	if (body.is_state_zero and state==0) or (not body.is_state_zero and state == 1):
-		game_manager.add_point()
-		animation_player.play("pickup")
+		game_manager.spawn_point = self.global_position
