36ff640101ebc7aae3ebc113a212c0bc764dfd4d
Mankrit Singh <singhmankrit@gmail.com>
2025-09-11 15:38:53 +0200

    comment

--- platformer_test/scripts/measurement_pad.gd
+++ platformer_test/scripts/measurement_pad.gd
@@ -6,7 +6,6 @@ var pos = self.global_position
 
 func _on_body_entered(body: Node2D) -> void:
 	# Detects if player touches measurement pad
-	print("Entered area")
 	var state = game_manager.measure()
 	if (body.is_state_zero and state==0) or (not body.is_state_zero and state == 1):
 		game_manager.spawn_pos = pos
