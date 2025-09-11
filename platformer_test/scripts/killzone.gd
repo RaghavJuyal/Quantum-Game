9b8d6daa23306a7fa1fac4b60a8ebec6a02594c8
Mankrit Singh <singhmankrit@gmail.com>
2025-09-11 11:40:20 +0200

    minor clean

--- platformer_test/scripts/killzone.gd
+++ platformer_test/scripts/killzone.gd
@@ -1,8 +1,7 @@
 extends Area2D
 
 @onready var timer: Timer = $Timer
-@onready var game_manager: Node = get_tree().root.get_node("Game/GameManager")
-
+@onready var game_manager: Node = %GameManager
 
 func _on_body_entered(body: Node2D) -> void:
 	var state = game_manager.measure()
--- platformer_test/scripts/slime_purple.gd
+++ platformer_test/scripts/slime_purple.gd
@@ -2,10 +2,10 @@ extends Node2D
 
 const SPEED = 60
 var direction = 1
+
 @onready var ray_cast_right: RayCast2D = $RayCastRight
 @onready var ray_cast_left: RayCast2D = $RayCastLeft
 @onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
