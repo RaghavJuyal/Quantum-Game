05a388d33cb7786a784c14017c5898dac88dea90
aronintendo <battarionwars@live.nl>
2025-09-25 14:48:23 +0200

    Dialog
    Dialog has been finished. Everything will be read from a single JSON for now. Only in level_manager do functions exist to stop the player characters. Perhaps this should be done in game_manager as well

--- platformer_test/scenes/texts.gd
+++ platformer_test/scenes/texts.gd
@@ -3,23 +3,26 @@ extends TextEdit
 @onready var label: Label = $MarginContainer/Label
 var text_name = "dialogue0"
 var filepath = "res://scripts/dialogue.json"
-var parsedDict
+var parsedResult
 
 func _ready() -> void:
-	label.text="Text is editable"
-	parsedDict = load_json(filepath)
+	label.text = "Text is editable"
+	load_json(filepath)
 	
-func load_json(filepath: String):
-	var parsedResult
+func load_json(filepath: String) -> void:
 	if FileAccess.file_exists(filepath):
 		var dataFile = FileAccess.open(filepath, FileAccess.READ)
 		parsedResult = JSON.parse_string(dataFile.get_as_text())
-		#print(parsedResult["dialogue0"][0]["0"])
+		print(parsedResult["dialogue1"])
+		#print(type_string(typeof(parsedResult["dialogue0"][0]["0"])))
 	else:
 		#print("file not found")
 		parsedResult = "No JSON"
-	return parsedResult
 
-func dialogreader(dialogname):
-	var index = 0
-	parsedDict[dialogname][0][String(index)]
+func dialogreader(dialogname, index) -> void:
+	#print(type_string(typeof(parsedResult["dialogue0"][0]["0"])))
+	#print(index)
+	#continue editing this accordingly plz
+	#print(type_string(typeof(label.text)))
+	label.text = parsedResult[dialogname][index][str(index)]
+	#label.text = "Fuck this"
