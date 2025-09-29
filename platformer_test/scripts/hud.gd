e8f00b11187fdad6aa7528085b8a2eb87ff09043
Raghav Juyal <rjuyal2020@gmail.com>
2025-09-29 17:44:21 +0200

    Add heart to hud

--- platformer_test/scripts/hud.gd
+++ platformer_test/scripts/hud.gd
@@ -3,8 +3,10 @@ extends CanvasLayer
 @onready var coins_label: Label = $CoinsLabel
 @onready var zero_label: Label = $Percent0
 @onready var one_label: Label = $Percent1
+@onready var heart_label: Label = $HeartLabel
 
 func _ready() -> void:
 	coins_label.text = str(0)
 	zero_label.text = str(100.0)
 	one_label.text = str(0.0)
+	heart_label.text = str(3)
