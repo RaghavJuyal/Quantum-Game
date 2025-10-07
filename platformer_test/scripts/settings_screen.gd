18e1aa83d3301f5a5b1b707041db1d69a48c2989
aronintendo <battarionwars@live.nl>
2025-10-07 16:02:15 +0200

    volume in pause menu
    I have made the volume slider a separate scene to put in settings as well as the pause menu

--- platformer_test/scripts/settings_screen.gd
+++ platformer_test/scripts/settings_screen.gd
@@ -1,5 +1,5 @@
 extends Control
-@onready var h_slider: HSlider = $CanvasLayer/HSlider
+@onready var h_slider: HSlider = $CanvasLayer/Panel/HSlider
 
 func set_game_manager(manager: Node):
 	# Propagate to children that also have a set_game_manager method
